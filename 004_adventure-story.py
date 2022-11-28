0960ba7b879f53bd89ee9e37380966531fb579ce
copamech <114030705+copamech@users.noreply.github.com>
2022-11-28 10:32:03 -0800

    Create 004_adventure-story.py

---
+++ 004_adventure-story.py
@@ -0,0 +1,10 @@
+print("""== Welcome to your adventure simulator! ==
+I am going to ask you a bunch of questions and then create an epic story with you as the star!""")
+print()
+name = input("What is your name? ")
+enemy = input("What is your worst enemy’s name? ")
+power = input("What is your superpower? ")
+home = input("Where do you live? ")
+food = input("What is your favorite food? ")
+print()
+print("Hello", name, "! Your ability to", power, "will make sure you never have to look at", enemy, "again. Go eat", food, "as you walk down the streets of", home, "and use", power, "for good and not evil!")

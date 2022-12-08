1435ee6a1f3f9a878af4470907e917a015353f26
peircecolin <114030705+peircecolin@users.noreply.github.com>
2022-12-08 15:38:59 -0800

    Create 032_lists.py

---
+++ 032_lists.py
@@ -0,0 +1,7 @@
+import random
+
+greetingList = ["Hello", "Hola", "Bonjour", "Konnichiwa", "Guten Tag", "Asalaam alaikum"]
+
+greetingNum = random.randint(0,5)
+
+print(f"{greetingList[greetingNum]}")

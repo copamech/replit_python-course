d53507d797640333a3b59763f7129122432b25a4
peircecolin <114030705+peircecolin@users.noreply.github.com>
2022-12-09 10:56:48 -0800

    Create 040_dictionaries.py

---
+++ 040_dictionaries.py
@@ -0,0 +1,16 @@
+print("🌟Contact Card🌟")
+
+name = input("Input your name > ")
+
+dob = input("Input your date of birth > ")
+
+phone = input("Input your telephone number > ")
+
+email = input("Input your email > ")
+
+address = input("Input your address > ")
+
+contact = { "name" : name, "dob" : dob, "phone": phone, "email": email, "address": address }
+
+print()
+print(f"Name: {contact['name']}")
